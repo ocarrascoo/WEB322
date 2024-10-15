896acc2111372479074466528ff920f78210ad74
Omar Carrasco <ocarrasco@myseneca.ca>
2024-10-14 21:28:44 -0400

    Author

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca Academic Policy.  No part of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+
+Name: 
+Student ID: 156333221
+Date: October 14 2024
+Vercel Web App URL: 
+GitHub Repository URL: https://github.com/ocarrascoo/WEB322
+
+********************************************************************************/ 
+
 const express = require('express');
 const app = express();
 const path = require('path');
