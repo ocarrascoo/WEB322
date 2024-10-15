b46d04fecc36330dba64945385a2f3052c8e0017
Omar Carrasco <ocarrasco@myseneca.ca>
2024-10-14 21:24:52 -0400

    Adding the routes

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.redirect('/about');
+});
+
+const storeService = require('./store-service');
+
+const PORT = process.env.PORT || 8080;
+
+storeService.initialize().then(() => {
+    
+    app.get('/shop', (req, res) => {
+        storeService.getPublishedItems().then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            res.json({ message: err });
+        });
+    });
+
+    app.get('/items', (req, res) => {
+        storeService.getAllItems().then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            res.json({ message: err });
+        });
+    });
+
+    app.get('/categories', (req, res) => {
+        storeService.getCategories().then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            res.json({ message: err });
+        });
+    });
+
+    app.use((req, res) => {
+        res.status(404).send('Page Not Found');
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Express http server listening on port ${PORT}`);
+    });
+
+}).catch((err) => {
+    console.log("Failed to initialize store-service: " + err);
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
